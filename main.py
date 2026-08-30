0ad5f70f06f73a7f71c901500c3d9f2f6dbdf425
Srivatsa C M <srivatsacm92@gmail.com>
2026-08-30 13:23:43 +0530

    Add professional command line interface

--- main.py
+++ main.py
@@ -1,30 +1,57 @@
-import sys
+import argparse
 
 from scanner.scanner import CodeScanner
 from scanner.reporter import ReportGenerator
 from scanner.json_reporter import JSONReporter
 
 
-if len(sys.argv) != 2:
-    print("Usage: python main.py <python_file>")
-    sys.exit(1)
+def main():
+
+    parser = argparse.ArgumentParser(
+        description="CodeGuard - Python Static Code Vulnerability Scanner"
+    )
+
+    parser.add_argument(
+        "file",
+        help="Path to the Python file to scan"
+    )
+
+    parser.add_argument(
+        "--json",
+        action="store_true",
+        help="Generate a JSON security report"
+    )
+
+    parser.add_argument(
+        "--output",
+        default="codeguard_report.json",
+        help="JSON output filename"
+    )
+
+    args = parser.parse_args()
+
+    file_path = args.file
+
+    scanner = CodeScanner()
+
+    if not scanner.validate_file(file_path):
+        return
+
+    findings = scanner.scan(file_path)
+
+    reporter = ReportGenerator()
+    reporter.generate(findings, file_path)
+
+    if args.json:
+
+        json_reporter = JSONReporter()
+
+        json_reporter.generate(
+            findings,
+            file_path,
+            args.output
+        )
 
 
-file_path = sys.argv[1]
-
-scanner = CodeScanner()
-
-if not scanner.validate_file(file_path):
-    sys.exit(1)
-
-findings = scanner.scan(file_path)
-
-reporter = ReportGenerator()
-reporter.generate(findings, file_path)
-
-json_reporter = JSONReporter()
-json_reporter.generate(
-    findings,
-    file_path,
-    "codeguard_report.json"
-)
+if __name__ == "__main__":
+    main()
